8d98bfd3fd8a3fb4560e0aa2c44b51da056b2614
Dwi Pasca <dpnursanti@gmail.com>
2021-08-19 19:30:05 +0800

    feat: create track item skeleton

--- src/components/trackItemSkeleton/index.tsx
+++ src/components/trackItemSkeleton/index.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 
+import { Flex, Box, SkeletonText } from "@chakra-ui/react";
+
 const TrackItemSkeleton = () => {
   return (
-    <div>
-      <p>Test</p>
-    </div>
+    <Flex flexDir="column" pl="20" pr="20">
+      <Box padding="6" boxShadow="lg" bg="white" m={2}>
+        <SkeletonText mt="4" noOfLines={2} spacing="2" />
+      </Box>
+      <Box padding="6" boxShadow="lg" bg="white" m={2}>
+        <SkeletonText mt="4" noOfLines={2} spacing="2" />
+      </Box>
+      <Box padding="6" boxShadow="lg" bg="white" m={2}>
+        <SkeletonText mt="4" noOfLines={2} spacing="2" />
+      </Box>
+      <Box padding="6" boxShadow="lg" bg="white" m={2}>
+        <SkeletonText mt="4" noOfLines={2} spacing="2" />
+      </Box>
+      <Box padding="6" boxShadow="lg" bg="white" m={2}>
+        <SkeletonText mt="4" noOfLines={2} spacing="2" />
+      </Box>
+    </Flex>
   );
 };
 
