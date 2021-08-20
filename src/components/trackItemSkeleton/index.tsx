9b7b96310c82351229bb3aa8ae34eb3c117edcc5
Dwi Pasca <dpnursanti@gmail.com>
2021-08-20 10:24:30 +0800

    fix: change mode skeleton

--- src/components/trackItemSkeleton/index.tsx
+++ src/components/trackItemSkeleton/index.tsx
@@ -1,26 +1,27 @@
 import React from "react";
 
-import { Flex, Box, SkeletonText } from "@chakra-ui/react";
+import { Grid, Box, SkeletonText, useColorModeValue } from "@chakra-ui/react";
 
 const TrackItemSkeleton = () => {
+  const skeletonStyle = useColorModeValue("gray.100", "gray.600");
   return (
-    <Flex flexDir="column" pl="20" pr="20">
-      <Box padding="6" boxShadow="lg" bg="white" m={2}>
+    <Grid flexDir="column" pl="20" pr="20">
+      <Box padding="6" boxShadow="lg" bg={skeletonStyle} m={2}>
         <SkeletonText mt="4" noOfLines={2} spacing="2" />
       </Box>
-      <Box padding="6" boxShadow="lg" bg="white" m={2}>
+      <Box padding="6" boxShadow="lg" bg={skeletonStyle} m={2}>
         <SkeletonText mt="4" noOfLines={2} spacing="2" />
       </Box>
-      <Box padding="6" boxShadow="lg" bg="white" m={2}>
+      <Box padding="6" boxShadow="lg" bg={skeletonStyle} m={2}>
         <SkeletonText mt="4" noOfLines={2} spacing="2" />
       </Box>
-      <Box padding="6" boxShadow="lg" bg="white" m={2}>
+      <Box padding="6" boxShadow="lg" bg={skeletonStyle} m={2}>
         <SkeletonText mt="4" noOfLines={2} spacing="2" />
       </Box>
-      <Box padding="6" boxShadow="lg" bg="white" m={2}>
+      <Box padding="6" boxShadow="lg" bg={skeletonStyle} m={2}>
         <SkeletonText mt="4" noOfLines={2} spacing="2" />
       </Box>
-    </Flex>
+    </Grid>
   );
 };
 
